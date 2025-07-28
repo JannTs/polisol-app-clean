d4a22bd7273fe3d89751cf907189c7a107f90f21
Evgenii Tsapko <yevgeni.tsapko@gmail.com>
2025-07-28 02:08:03 +0200

    store

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const EcwidStorefront = dynamic(
 export default function Home() {
   return (
     <main style={{ padding: '2rem' }}>
-      <h1>Добро пожаловать в Polisol Store</h1>
+      <h1>Добро пожаловать в Store</h1>
       <EcwidStorefront storeId="10995149" layout="grid" />
     </main>
   );
