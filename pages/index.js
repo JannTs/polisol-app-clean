b8aa845d1d8e6e847579fa819971e04a7c021134
Evgenii Tsapko <yevgeni.tsapko@gmail.com>
2025-07-28 02:34:51 +0200

    tempor

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
-// pages/index.js
-import dynamic from 'next/dynamic';
-
-const EcwidStorefront = dynamic(
-  () => import('@ecwid/nextjs-ecwid-plugin').then(mod => mod.EcwidStorefront),
-  { ssr: false }
-);
+import Script from 'next/script';
 
 export default function Home() {
   return (
     <main style={{ padding: '2rem' }}>
-      <h1>Добро пожаловать в Store</h1>
-      <EcwidStorefront storeId="10995149" layout="grid" />
+      <h1>Ecwid встроенный вручную</h1>
+
+      <div id="my-store-10995149"></div>
+      <div>
+        <Script
+          id="ecwid-script"
+          src="https://app.ecwid.com/script.js?10995149&data_platform=code&data_date=2025-07-27"
+          type="text/javascript"
+          strategy="afterInteractive"
+        />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-10995149");`,
+          }}
+        />
+      </div>
     </main>
   );
 }
